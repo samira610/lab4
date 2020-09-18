9eab7439ccb83558a166289dbde1e94801fab77c
unknown <samiraee@gmail.com>
2020-09-18 17:40:16 +0200

    :)

---
+++ R/lab4p-internal.R
@@ -0,0 +1,12 @@
+#' @title linreg
+#' @details Multiple regression model.
+#' @description .
+#' @param a A formula.
+#' @param b A data.
+#' @return An object with of class linreg either as an S3 class or an RC class \code{a}.
+#' @seealso \url{https://en.wikipedia.org/wiki/Euclidean_algorithm}
+#' @export
+
+linreg <- function(a,b){
+  
+}
