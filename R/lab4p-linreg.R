8ad67d5c2fb7d339105b583b200ee49337c850d1
unknown <samiraee@gmail.com>
2020-09-21 19:48:59 +0200

    details2

--- R/lab4p-linreg.R
+++ R/lab4p-linreg.R
@@ -1,12 +1,13 @@
 #' @title linreg
 #' @details Multiple regression model.
-#' @description .
-#' @param a A formula.
-#' @param b A data.
+#' @description The code for a multiple regression model. 
+#' Linreg have the two arguments formula and data.
+#' The function return an object with of class linreg either as an S3 class or an RC class.
+#' @param f A formula.
+#' @param d A data.
 #' @return An object with of class linreg either as an S3 class or an RC class \code{a}.
-#' @seealso \url{https://en.wikipedia.org/wiki/Euclidean_algorithm}
 #' @export
 
-linreg <- function(a,b){
+linreg <- function(f,d){
   
 }
